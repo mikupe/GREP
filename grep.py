5e2a0600fd472f0869271b088a4f0cf5daafd821
Petr Ming <petrming@jupyter.korpus.cz>
2018-03-15 17:10:21 +0100

    Code's been wrapped in functions!

--- grep.py
+++ grep.py
@@ -16,21 +16,32 @@ except ModuleNotFoundError:
     print("regex not module not found, you are currently using re module", file=sys.stderr)
     import re
 
-
+#hodit do fce
 #selže přiřazování argumentů
-try:
-    pattern, path = sys.argv[1:]
-except ValueError:
-    print(__doc__.strip(), file=sys.stderr)
-    sys.exit(1)
+
+
+
+def parse_argv(argv):
+    try:
+        pattern, path = argv
+        return pattern, path
+    except ValueError:
+        print(__doc__.strip(), file=sys.stderr)
+        sys.exit(1)
     
-    
+#hodit do fce
 #selže otevírání souborů
-try:
-    with open(path) as f:
-        for line in f:
-            if re.search(pattern,line):
-                print(line, end="")
-except FileNotFoundError:
-    print("file not found:", path, file=sys.stderr)
-    sys.exit(1)
+
+def main(argv):
+    pattern, path = parse_argv(argv)
+    try:
+        with open(path) as f:
+            for line in f:
+                if re.search(pattern,line):
+                    print(line, end="")
+    except FileNotFoundError:
+        print("file not found:", path, file=sys.stderr)
+        sys.exit(1)
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
